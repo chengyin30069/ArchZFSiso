79d778e3ecbe2f036e224be2e1cbe631896af112
Linus <chengyin30069@gmail.com>
2026-05-10 00:51:48 +0800

    GTG!

--- build.sh
+++ build.sh
@@ -2,40 +2,46 @@
 
 sudo pacman -Syu --noconfirm archiso base-devel git
 
+cd $1
 
+CONFIG_DIR=$(pwd)
+echo All iso config in ${CONFIG_DIR}/ISOBUILD
 
-cd 
+cd $CONFIG_DIR
 rm -rf ISOBUILD && mkdir ISOBUILD
 cp -r /usr/share/archiso/configs/releng/ ISOBUILD/ && mv ISOBUILD/releng ISOBUILD/zfsiso
 
-cd ~/ISOBUILD
+cd ${CONFIG_DIR}/ISOBUILD
 git clone https://aur.archlinux.org/zfs-dkms.git && \
 	cd zfs-dkms && \
 	makepkg --skippgpcheck
 
-cd ~/ISOBUILD
+cd ${CONFIG_DIR}/ISOBUILD
 git clone https://aur.archlinux.org/zfs-utils.git && \
 	cd zfs-utils && \
 	makepkg --skippgpcheck
 
-cd ~/ISOBUILD/zfsiso && mkdir zfsrepo && cd zfsrepo && \
-	cp ~/ISOBUILD/zfs-dkms/*.zst . && \
-	cp ~/ISOBUILD/zfs-utils/*.zst . && \
+cd ${CONFIG_DIR}/ISOBUILD/zfsiso && mkdir zfsrepo && cd zfsrepo && \
+	cp ${CONFIG_DIR}/ISOBUILD/zfs-dkms/*.zst . && \
+	cp ${CONFIG_DIR}/ISOBUILD/zfs-utils/*.zst . && \
 	repo-add zfsrepo.db.tar.gz *.zst
 
-echo "# ZFS custom repo" >> ~/ISOBUILD/zfsiso/packages.x86_64 && \
-    echo "linux-headers" >> ~/ISOBUILD/zfsiso/packages.x86_64 && \
-    echo "zfs-dkms" >> ~/ISOBUILD/zfsiso/packages.x86_64 && \
-    echo "zfs-utils" >> ~/ISOBUILD/zfsiso/packages.x86_64 && \
-	echo "fastfetch" >> ~/ISOBUILD/zfsiso/packages.x86_64
+echo "# ZFS custom repo" >> ${CONFIG_DIR}/ISOBUILD/zfsiso/packages.x86_64 && \
+    echo "linux-headers" >> ${CONFIG_DIR}/ISOBUILD/zfsiso/packages.x86_64 && \
+    echo "zfs-dkms" >> ${CONFIG_DIR}/ISOBUILD/zfsiso/packages.x86_64 && \
+    echo "zfs-utils" >> ${CONFIG_DIR}/ISOBUILD/zfsiso/packages.x86_64 && \
+	echo "fastfetch" >> ${CONFIG_DIR}/ISOBUILD/zfsiso/packages.x86_64
 
-cd ~/ISOBUILD/zfsiso && \
+cd ${CONFIG_DIR}/ISOBUILD/zfsiso && \
     echo "#" && \
     echo "[zfsrepo]" >> pacman.conf && \
     echo "SigLevel = Optional TrustAll" >> pacman.conf && \
-    echo "Server = file:///ISOBUILD/zfsiso/zfsrepo" >> pacman.conf
+    echo "Server = file://${CONFIG_DIR}/ISOBUILD/zfsiso/zfsrepo" >> pacman.conf
 
-RUN mkdir -p ~/ISOBUILD/zfsiso/{WORK,ISOOUT}
+mkdir -p ${CONFIG_DIR}/ISOBUILD/zfsiso/{WORK,ISOOUT}
 
-sudo mkarchiso -v -w ~/ISOBUILD/zfsiso/WORK -o ~/ISOBUILD/zfsiso/ISOOUT zfsiso/
+sudo mkarchiso -v -w ${CONFIG_DIR}/ISOBUILD/zfsiso/WORK -o ${CONFIG_DIR}/ISOBUILD/zfsiso/ISOOUT ${CONFIG_DIR}/ISOBUILD/zfsiso/
 
+if [ $? -eq 0 ]; then
+	echo Build finished, iso in ${CONFIG_DIR}/ISOBUILD/zfsiso/ISOOUT
+fi
